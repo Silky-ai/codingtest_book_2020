aa1ba2b0547309d16ff86e2c324516bb74fdc884
GRAIM-ai <59557453+GRAIM-ai@users.noreply.github.com>
2021-01-29 01:11:15 +0900

    Update 숨바꼭질.py

--- 최단 경로/숨바꼭질.py
+++ 최단 경로/숨바꼭질.py
@@ -1,3 +1,5 @@
+import heapq
+
 n,m=list(map(int,input().split()))
 
 INF=int(1e9)
@@ -10,17 +12,27 @@ for _ in range(m):
     graph[temp[0]][temp[1]]=1
     graph[temp[1]][temp[0]]=1
 
+distance=[INF]*(n+1)
+q=[]
+start=1
+heapq.heappush(q,(0,start))
+while (q!=[]):
+    dist,current=heapq.heappop(q)
+    if(dist>distance[current]):
+        continue
+    for i in range(1,n+1):
+        cost=dist+graph[current][i]
+        if(cost<distance[i]):
+            distance[i]=cost
+            heapq.heappush(q,(cost,i))
 
-for k in range(1,n+1):
-    for a in range(1,n+1):
-        for b in range(1,n+1):
-            graph[a][b]=min(graph[a][b],graph[a][k]+graph[k][b])
 ans=[]
-maxx=max(graph[1][1:])
+
+maxx=max(distance[1:])
 for i in range(1,n+1):
-    if(graph[1][i]==maxx):
+    if(distance[i]==maxx):
         ans.append(i)
 ans.sort()
 print(ans[0])
-print(graph[1][ans[0]])
+print(maxx)
 print(len(ans))
